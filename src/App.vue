206c19ae29a03108a1e11ce8db5aaf29ec1f3821
hhcherry <104436626h@gmail.com>
2021-01-11 22:05:25 +0800

    columnlist页面封装

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App    "/>
+ <columnList :list=list></columnList>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import columnList, { ColumnProps } from './components/columnList'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'vue3',
+    avatar: 'http://placehold.it/50x50',
+    description: '使用vue3编写仿知乎功能'
+  },
+  {
+    id: 2,
+    title: 'vue3',
+    avatar: 'http://placehold.it/50x50',
+    description: '使用vue3编写仿知乎功能'
+  }
+]
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    columnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
+
 })
 </script>
 
---
+++ src/components/columnList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="item in list" :key="item.id">
+      <img :src="item.avatar" :alt="item.title">
+      <h3>{{item.title}}</h3>
+      <h6>{{item.description}}</h6>
+      <a href="#" target="_blank" rel="noopener noreferrer">查看详情</a>
+      </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      require: true
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
