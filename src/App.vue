74682183083c686219440b551647fcc48c02f63b
hhcherry <104436626h@gmail.com>
2021-01-13 22:50:43 +0800

    bootstrap样式引用

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
- <columnList :list=list></columnList>
+  <div class="container">
+    <Nav :user=user></Nav>
+    <columnList :list=list></columnList>
+ </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import columnList, { ColumnProps } from './components/columnList'
+import columnList, { ColumnProps } from './components/columnList.vue'
+import Nav, { UserInfo } from './components/nav.vue'
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -18,16 +21,35 @@ const testData: ColumnProps[] = [
     title: 'vue3',
     avatar: 'http://placehold.it/50x50',
     description: '使用vue3编写仿知乎功能'
+  },
+  {
+    id: 1,
+    title: 'vue3',
+    // avatar: 'http://placehold.it/50x50',
+    description: '使用vue3编写仿知乎功能'
+  },
+  {
+    id: 2,
+    title: 'vue3',
+    avatar: 'http://placehold.it/50x50',
+    description: '使用vue3编写仿知乎功能'
   }
 ]
+const testUser: UserInfo = {
+  isLogin: false,
+  name: 'lisha',
+  id: 'lili'
+}
 export default defineComponent({
   name: 'App',
   components: {
-    columnList
+    columnList,
+    Nav
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      user: testUser
     }
   }
 
@@ -41,6 +63,6 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
